de8a0d0c1486d83c1504d0da9ec4532111cdd4c3
Nancy Jo Chang <enciie@gmail.com>
2018-04-02 17:12:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function takeANumber(katzDeliLine, name){
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
 }
 function nowServing(katzDeliLine){
-  if (katzDeliLine.length === 0){
+  if (!katzDeliLine.length){
     //if nobody is in line katzDeliLine.length will equal to 0
   return "There is nobody waiting to be served!";  
   } else {
